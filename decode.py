f6d74906b69bfac63bb6f13bd8517677947255a1
unknown <hong.h@ufl.edu>
2024-07-24 17:55:33 -0400

    New file create

---
+++ decode.py
@@ -0,0 +1,19 @@
+'''
+implement your decode function here
+'''
+
+
+
+
+# def decode(password):
+#
+#     decoded_password = []  # Create empty list
+#     for char in password:
+#         if char.isdigit():
+#             # Subtract 3 from the digit and wrap around using modulo 10
+#             decoded_char = (int(char) - 3) % 10
+#             decoded_password.append(str(decoded_char))
+#         else:
+#             raise ValueError("Password must contain only numeric characters.")
+#     return ''.join(decoded_password)
+#
